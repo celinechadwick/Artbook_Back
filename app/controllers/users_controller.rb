5780d9487b82e346144e2d46514b938aea3ac8dd
General Assembly <cvc1230@gmail.com>
2017-07-19 10:08:02 -0400

    Remove phone_number from strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
       def user_params
         params
         .require(:user)
-        .permit(:first_name, :last_name, :phone_number, :email, :password)
+        .permit(:first_name, :last_name, :email, :password)
       end
     end
